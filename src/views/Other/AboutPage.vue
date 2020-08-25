3e0327ff52221c5910d71f77ad95f324f8765f7c
Luc Abayo <luc.bayo@gmail.com>
2020-08-25 11:12:55 +0200

    Update AboutPage.vue

--- src/views/Other/AboutPage.vue
+++ src/views/Other/AboutPage.vue
@@ -28,16 +28,16 @@
                   preserveAspectRatio="xMidYMid slice"
                   focusable="false"
                   role="img"
-                  aria-label="Placeholder: Thumbnail"
+                  aria-label="Placeholder: Developer"
                 >
                   <title>Placeholder</title>
                   <rect width="100%" height="100%" fill="#55595c" />
-                  <text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text>
+                  <text x="50%" y="50%" fill="#eceeef" dy=".3em">Author</text>
                 </svg>
                 <div class="card-body">
                   <p
                     class="card-text"
-                  >This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                  >Gakunde Prayer</p>
                   <div class="d-flex justify-content-between align-items-center">
                     <div class="btn-group">
                       <a href="#" class="btn btn-sm btn-outline-primary">LinkdIn</a>
@@ -59,11 +59,11 @@
                   preserveAspectRatio="xMidYMid slice"
                   focusable="false"
                   role="img"
-                  aria-label="Placeholder: Thumbnail"
+                  aria-label="Placeholder: Developer"
                 >
                   <title>Placeholder</title>
                   <rect width="100%" height="100%" fill="#55595c" />
-                  <text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text>
+                  <text x="50%" y="50%" fill="#eceeef" dy=".3em">Developer</text>
                 </svg>
                 <div class="card-body">
                   <p
